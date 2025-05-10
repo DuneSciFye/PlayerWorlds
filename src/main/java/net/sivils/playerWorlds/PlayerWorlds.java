167fea40ed18483443ba7c8b63592ff61639d2d1
dunescifye <dunescifye@gmail.com>
2025-05-10 11:40:11 -0400

    Creating Repository

---
+++ src/main/java/net/sivils/playerWorlds/PlayerWorlds.java
@@ -0,0 +1,17 @@
+package net.sivils.playerWorlds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerWorlds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
